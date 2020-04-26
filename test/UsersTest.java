d94659cfe95aa5b23bff05c414db5b507c8c2ab9
unknown <Vitalie@WS-IT-SARI-03.112MD.local>
2020-04-26 15:30:19 +0300

    No persistence EntManager found

---
+++ test/UsersTest.java
@@ -0,0 +1,25 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.bookstore.entity.Users;
+
+public class UsersTest {
+
+	public static void main(String[] args) {
+		Users user1 = new Users();
+		user1.setEmail("nam@codeajva.net");
+		user1.setFullName("Nam Ha Minh");
+		user1.setPassword("helloworld");
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("BookStoreWebsite");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		entityManager.persist(user1);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		entityManagerFactory.close();
+		System.out.println("A user object was persisted");
+
+	}
+
+}
