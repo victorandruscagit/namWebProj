86e07348f2c42dcc84fc48e9079230a7207ae062
victorandruscabit <victor.v.andrusca@gmail.com>
2020-05-12 16:19:39 +0300

    First commit for bit

--- test/UsersTest.java
+++ test/UsersTest.java
@@ -1,16 +1,15 @@
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
 import com.bookstore.entity.Users;
 
 public class UsersTest {
 
 	public static void main(String[] args) {
-		Users user1 = new Users();
-		user1.setEmail("nam@codeajva.net");
-		user1.setFullName("Nam Ha Minh");
-		user1.setPassword("helloworld");
+		Users user1 = new com.bookstore.entity.Users();
+		user1.setEmail("sofia@extendaretail.net");
+		user1.setFullName("Miss Sofia");
+		user1.setPassword("extenda");
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("BookStoreWebsite");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
